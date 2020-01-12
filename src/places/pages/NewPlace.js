cf298431b53f1f9d966ac6f305e09d7e2b3f34ee
Gavin Williams <gamichwi@gmail.com>
2020-01-12 21:57:42 +1100

    added description field to new place component. Set up validation handling.

--- src/places/pages/NewPlace.js
+++ src/places/pages/NewPlace.js
@@ -1,13 +1,41 @@
-import React from 'react';
+import React, { useCallback } from "react";
 
-import Input from '../../shared/components/FormElements/Input'
+import Input from "../../shared/components/FormElements/Input";
+import {
+  VALIDATOR_REQUIRE,
+  VALIDATOR_MINLENGTH
+} from "../../shared/util/validators";
 
-import './NewPlace.css'
+import "./NewPlace.css";
 
 const NewPlace = () => {
-    return <form className="place-form">
-        <Input type="text" label="Title" />
+  const titleInputHandler = useCallback((id, value, isValid) => {}, []);
+
+  const descriptionInputHandler = useCallback((id, value, isValid) => {}, []);
+
+
+  return (
+    <form className="place-form">
+      <Input
+        id="title"
+        element="input"
+        type="text"
+        label="Title"
+        validators={[VALIDATOR_REQUIRE()]}
+        errorText="Please enter a valid title."
+        onInput={titleInputHandler}
+      />
+      <Input
+        id="description"
+        element="textarea"
+        type="text"
+        label="Description"
+        validators={[VALIDATOR_MINLENGTH(5)]}
+        errorText="Please enter a valid description (at least 5 characters)."
+        onInput={descriptionInputHandler}
+      />
     </form>
-}
+  );
+};
 
 export default NewPlace;
