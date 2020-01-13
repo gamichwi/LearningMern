8bb50ade6b79440761b63af9d282ef0f34b3fa72
Gavin Williams <gamichwi@gmail.com>
2020-01-13 16:47:07 +1100

    Form validation updated. Validation now covers both description and title with less code. Button added based on validation outcome (true/false)

--- src/shared/components/UIElements/Map.js
+++ src/shared/components/UIElements/Map.js
@@ -1,20 +1,20 @@
-import React, { useRef, useEffect } from "react";
+import React, { useRef, useEffect } from 'react';
 
-import "./Map.css";
+import './Map.css';
 
 const Map = props => {
-  const mapRef = useRef();    
+  const mapRef = useRef();
   
   const { center, zoom } = props;
 
   useEffect(() => {
     const map = new window.google.maps.Map(mapRef.current, {
-      center: props.center,
-      zoom: props.zoom
+      center: center,
+      zoom: zoom
     });
-
-    new window.google.maps.Marker({ position: props.center, map: map });
-  }, [center, zoom]);
+  
+    new window.google.maps.Marker({ position: center, map: map });
+  }, [center, zoom]);  
 
   return (
     <div
